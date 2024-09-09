2846eb0e479b712f10c5b6c26a0ec43f031edb8a
Pramodjakkam <99605657+Pramodjakkam@users.noreply.github.com>
2024-09-09 15:07:17 +0530

    commited the changes
    First commit if any updates or changes can be done later on respective update.

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/app/**/*.{js,ts,jsx,tsx}", // Include the `src/app` directory
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
